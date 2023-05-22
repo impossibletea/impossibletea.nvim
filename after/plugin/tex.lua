99840295f4b34ef3852d6dc5c8764aa1e07eb15c
Zakhar Tarshis <ztarshis@tradingview.com>
2023-05-22 19:25:25 +0300

    Change vimtex regex highlight to treesitter

--- after/plugin/tex.lua
+++ after/plugin/tex.lua
@@ -3,3 +3,4 @@ require('lspconfig')['texlab'].setup{
 }
 
 vim.g.vimtex_view_method = 'zathura'
+vim.g.vimtex_syntax_enabled = false
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -8,9 +8,6 @@ require'nvim-treesitter.configs'.setup{
    },
    highlight = {
       enable = true,
-      disable = {
-         'latex',
-      },
       additional_vim_regex_highlighting = false,
    },
 }
