837cec18da65e37b46ece745892ed5def202f5e7
Zakhar Tarshis <ztarshis@tradingview.com>
2023-09-22 10:50:04 +0300

    Shrink textwidth

--- lua/impossibletea/settings.lua
+++ lua/impossibletea/settings.lua
@@ -2,7 +2,7 @@ vim.cmd.colorscheme('rams')
 vim.opt.colorcolumn = '80'
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
-vim.opt.textwidth = 80
+vim.opt.textwidth = 78
 
 vim.opt.cursorline = true
 vim.opt.number = true
